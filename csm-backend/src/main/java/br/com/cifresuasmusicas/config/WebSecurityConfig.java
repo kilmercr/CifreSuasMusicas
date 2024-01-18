78006605771b625f672cdbf9c01ff00b730ba56d
kilmercr <kilmer.souza@gmail.com>
2024-01-18 18:47:36 -0300

    Initial Commit

--- csm-backend/src/main/java/br/com/cifresuasmusicas/config/WebSecurityConfig.java
+++ csm-backend/src/main/java/br/com/cifresuasmusicas/config/WebSecurityConfig.java
@@ -6,39 +6,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
-
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-            .csrf(AbstractHttpConfigurer::disable)
-            .exceptionHandling(except -> except.accessDeniedHandler(accessDeniedHandler()))
-            .authorizeHttpRequests(auth ->
-                auth.requestMatchers(antMatcher("/"), antMatcher("/h2-console/**")).permitAll()
-                    .anyRequest().authenticated())
-            .httpBasic(Customizer.withDefaults());
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.exceptionHandling(except -> except.accessDeniedHandler(accessDeniedHandler()))
+			.authorizeHttpRequests(auth ->
+				auth.requestMatchers(antMatcher("/"), antMatcher("/h2-console/**")).permitAll()
+					.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults());
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return new LoggingAccessDeniedHandler();
-    }
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		return new LoggingAccessDeniedHandler();
+	}
 }
